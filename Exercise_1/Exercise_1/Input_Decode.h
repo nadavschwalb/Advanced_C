a84504038fd6b45f9d4cfa58cee88cc08ec49732
shoval ben shushan <shovalb2@mail.tau.ac.il>
2020-10-25 12:50:13 +0200

    added - test.c command_executor

--- Exercise_1/Exercise_1/Input_Decode.h
+++ Exercise_1/Exercise_1/Input_Decode.h
@@ -1,1 +1,3 @@
 #pragma once
+
+int command_exeutor(Linked_List* list, char* input_command, int i, int j);
--- Exercise_1/Exercise_1/main.c
+++ Exercise_1/Exercise_1/main.c
@@ -4,45 +4,45 @@
 
 
 
-int main(int argc, char** argv) {
-
-	Node* head = creat_new_node(1);
-	Linked_List* list = init_list(head);
-	print_list(list);
-	Node* new_node = creat_new_node(2);
-	list = append_node(list, new_node);
-	print_list(list);
-	new_node = creat_new_node(3);
-	list = append_node(list, new_node);
-	print_list(list);
-	new_node = creat_new_node(5);
-	list = append_node(list, new_node);
-	print_list(list);
-	new_node = creat_new_node(19);
-	list = push_node(list, new_node);
-	print_list(list);
-	new_node = creat_new_node(10);
-	list = push_node(list, new_node);
-	print_list(list);
-	new_node = creat_new_node(16);
-	list = append_node(list, new_node);
-	print_list(list);
-	new_node = creat_new_node(21);
-	list = insert_after(list, 10, new_node);
-	print_list(list);
-	print_index(list, 16);
-	print_index(list, 23);
-	print_index(list, 10);
-	print_list(list);
-	remove_node(list, 0);
-	print_list(list);
-	remove_node(list, 6);
-	print_list(list);
-	remove_node(list, 4);
-	print_list(list);
-	remove_node(list, 6);
-	print_list(list);
-
-	if(linked_list_cleanup(list)) return 0;
-	else return(-1);
-}
+//int main(int argc, char** argv) {
+//
+//	node* head = creat_new_node(1);
+//	linked_list* list = init_list(head);
+//	print_list(list);
+//	node* new_node = creat_new_node(2);
+//	list = append_node(list, new_node);
+//	print_list(list);
+//	new_node = creat_new_node(3);
+//	list = append_node(list, new_node);
+//	print_list(list);
+//	new_node = creat_new_node(5);
+//	list = append_node(list, new_node);
+//	print_list(list);
+//	new_node = creat_new_node(19);
+//	list = push_node(list, new_node);
+//	print_list(list);
+//	new_node = creat_new_node(10);
+//	list = push_node(list, new_node);
+//	print_list(list);
+//	new_node = creat_new_node(16);
+//	list = append_node(list, new_node);
+//	print_list(list);
+//	new_node = creat_new_node(21);
+//	list = insert_after(list, 10, new_node);
+//	print_list(list);
+//	print_index(list, 16);
+//	print_index(list, 23);
+//	print_index(list, 10);
+//	print_list(list);
+//	remove_node(list, 0);
+//	print_list(list);
+//	remove_node(list, 6);
+//	print_list(list);
+//	remove_node(list, 4);
+//	print_list(list);
+//	remove_node(list, 6);
+//	print_list(list);
+//
+//	if(linked_list_cleanup(list)) return 0;
+//	else return(-1);
+//}
