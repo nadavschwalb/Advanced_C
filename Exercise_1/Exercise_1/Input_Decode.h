c1c5ef6bbf649233fb8434eaf3af22e7af5b70db
nadav sholev <nadavschwalb@mail.tau.ac.il>
2020-10-25 15:05:29 +0200

    exit sequence revert

--- Exercise_1/Exercise_1/Input_Decode.h
+++ Exercise_1/Exercise_1/Input_Decode.h
@@ -1,3 +1,3 @@
 #pragma once
 #include "Linked_List.h"
-void command_exeutor(Linked_List* list, char* input_command, int i, int j);
+int command_exeutor(Linked_List* list, char* input_command, int i, int j);
