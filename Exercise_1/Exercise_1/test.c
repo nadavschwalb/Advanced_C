4e4ae2c0e3ff56b3768c2f796e04c4effc58b52d
shoval ben shushan <shovalb2@mail.tau.ac.il>
2020-10-25 14:58:25 +0200

    exit sequence working program

--- Exercise_1/Exercise_1/test.c
+++ Exercise_1/Exercise_1/test.c
@@ -5,19 +5,21 @@
 
 #define MAX_INPUT_LINE_SIZE 100
 
-//int main(int argc, char** argv) {
-//	bool ex = false;
-//	char line[MAX_INPUT_LINE_SIZE];
-//	char command[MAX_INPUT_LINE_SIZE];
-//	int i,j=0;
-//	while (!ex) {
-//		printf("Enter command: \n");
-//		fgets(line, MAX_INPUT_LINE_SIZE, stdin);
-//		printf(line);
-//		sscanf(line, "%s %d %d \n", command, &i, &j);
-//		printf("\n string: %s \n num1: %d \n num2: %d \n", command, i, j);
-//		strlwr(command);
-//		printf("\n string low: %s \n", command);
-//	}
-//	return 0;
-//}
+bool exit = false;
+
+int main(int argc, char** argv) {
+	char line[MAX_INPUT_LINE_SIZE];
+	char command[MAX_INPUT_LINE_SIZE];
+	int i,j=0;
+	while (!exit) {
+		printf("Enter command: \n");
+		fgets(line, MAX_INPUT_LINE_SIZE, stdin);
+		printf(line);
+		sscanf(line, "%s %d %d \n", command, &i, &j);
+		printf("\n string: %s \n num1: %d \n num2: %d \n", command, i, j);
+		strlwr(command);
+		printf("\n string low: %s \n", command);
+
+	}
+	return 0;
+}
