85c8bac1f757aaa55ac7308b3012d5eab14a745a
shoval ben shushan <shovalb2@mail.tau.ac.il>
2020-10-26 11:07:19 +0200

    version before nadav destroied it

--- Exercise_1/Exercise_1/Input_Decode.h
+++ Exercise_1/Exercise_1/Input_Decode.h
@@ -1,3 +1,3 @@
 #pragma once
 #include "Linked_List.h"
-int command_exeutor(Linked_List* list, char* input_command, int i, int j);
+void command_exeutor(Linked_List* list, char* input_command, int i, int j);
