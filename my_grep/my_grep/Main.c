78a4b8bb73b8a10d1cc9b28bc6c1ac1984e95e70
nadav sholev <nadavschwalb@mail.tau.ac.il>
2020-11-10 15:02:35 +0200

    handle almost all inputs and read from file

--- my_grep/my_grep/Main.c
+++ my_grep/my_grep/Main.c
@@ -2,35 +2,43 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 #define MAXBUFF 4096
 int main(int argc, char** argv) {
-	int option_flag = 0x0; //options flag -i-v-n-b-x-A
+	int option_flag = 0x0; //options flag -i-v-n-b-x-A-c
 	int spacer_line_num = 0;
-	char file_name[MAXBUFF];
+	char file_name[MAXBUFF]; 
+	char line_buffer[MAXBUFF]; //holds line read from file
+	char search_buffer[MAXBUFF]; // holds sub string argument to search for in line
 	//TODO: get all command argumets
 	for (int i = 1; i < argc; i++) {
 		if (strchr(argv[i],'-') != NULL) { // option found
 			switch (*(argv[i]+1))
 			{
 			case 'i':
-				option_flag = option_flag | 0x100000;
+				option_flag = option_flag | 0xb1000000;
 				break;
 			case 'v':
-				option_flag = option_flag | 0x010000;
+				option_flag = option_flag | 0x0100000;
 				break;
 			case 'n':
-				option_flag = option_flag | 0x001000;
+				option_flag = option_flag | 0x0010000;
 				break;
 			case 'b':
-				option_flag = option_flag | 0x000100;
+				option_flag = option_flag | 0x0001000;
 				break;
 			case 'x':
-				option_flag = option_flag | 0x000010;
+				option_flag = option_flag | 0x0000100;
 				break;
 			case 'A':
-				option_flag = option_flag | 0x000001;
-				spacer_line_num = atoi(argv[i + 1]);
+				option_flag = option_flag | 0x0000010;
+				spacer_line_num = atoi(argv[i 
+					
+					+ 1]);
 				break;
+			case 'c':
+				option_flag = option_flag | 0x0000001;
+
 			default:
 				option_flag = option_flag | 0x0;
 				break;
@@ -38,8 +46,9 @@ int main(int argc, char** argv) {
 		}
 
 	}
+	strcpy(search_buffer, argv[argc - 2]);
 	strcpy(file_name, argv[argc - 1]);
-	printf("option flags: 0x%x -i-v-n-b-x-A\nNUM = %d\nfilename: %s\n", option_flag,spacer_line_num,file_name);
+	printf("option flags: 0x%x -i-v-n-b-x-A-c\nNUM = %d\nfilename: %s\n", option_flag,spacer_line_num,file_name);
 	
 	
 	//TODO: if no file given get stdin
@@ -50,12 +59,19 @@ int main(int argc, char** argv) {
 		printf("failed to open file %s\n", file_name);
 		exit(-1);
 	}
-	//TODO: get new line
-
-	//TODO: search for string in new line
+	//TODO: while get new line until EOF
+	while (fgets(line_buffer, MAXBUFF, p_input_file) != NULL) {
+		printf("input line : %s\n", line_buffer);
+	}
+	
 
 	//TODO: if found in 
 
+	//TODO: get line number
+
+	
+
+	//TODO: byte number
 
 	//TODO: cleanup
 	close(p_input_file);
