77208b10e953cbe8cd7d719ff42e58cfdfe46c76
nadav sholev <nadavschwalb@mail.tau.ac.il>
2020-11-21 12:36:42 +0200

    added stdin functionality still need to test it

--- my_grep/my_grep/Main.c
+++ my_grep/my_grep/Main.c
@@ -9,38 +9,47 @@
 
 int main(int argc, char** argv) {
 	int option_flag = 0b0; //options flag -i-v-n-b-x-A-c-E
+	int options_count = 0;
 	int spacer_line_num = 0;
 	char file_name[MAXBUFF];
 	char line_buffer[MAXBUFF]; //holds line read from file
 	char search_buffer[MAXBUFF]; // holds sub string argument to search for in line
-	//TODO: get all command argumets
+	FILE* p_input_file;
 	for (int i = 1; i < argc; i++) {
 		if (strchr(argv[i], '-') != NULL) { // option found
 			switch (*(argv[i] + 1))
 			{
 			case 'i':
 				option_flag = option_flag | _i;
+				options_count++;
 				break;
 			case 'v':
 				option_flag = option_flag | _v;
+				options_count++;
 				break;
 			case 'n':
 				option_flag = option_flag | _n;
+				options_count++;
 				break;
 			case 'b':
 				option_flag = option_flag | _b;
+				options_count++;
 				break;
 			case 'x':
 				option_flag = option_flag | _x;
+				options_count++;
 				break;
 			case 'A':
 				option_flag = option_flag | _A;
+				options_count+=2;
 				spacer_line_num = atoi(argv[i + 1]);
 				break;
 			case 'c':
 				option_flag = option_flag | _c;
+				options_count++;
 			case 'E':
 				option_flag = option_flag | _E;
+				options_count++;
 
 			default:
 				option_flag = option_flag | 0b0;
@@ -49,16 +58,24 @@ int main(int argc, char** argv) {
 		}
 
 	}
-	strcpy(search_buffer, argv[argc - 2]);
-	strcpy(file_name, argv[argc - 1]);
+	if (argc - options_count > 2) { //file given
+		strcpy(search_buffer, argv[argc - 2]);
+		strcpy(file_name, argv[argc - 1]);
+		p_input_file = fopen(file_name, "r");
+		if (p_input_file == NULL) {
+			printf("failed to open file %s\n", file_name);
+			exit(-1);
+		}
+	}
+	else {
+		strcpy(search_buffer, argv[argc - 1]);
+		p_input_file = stdin;
+	}
+	
 	//TODO: if no file given get stdin
 
 	//TODO: open input file
-	FILE* p_input_file = fopen(file_name, "r");
-	if (p_input_file == NULL) {
-		printf("failed to open file %s\n", file_name);
-		exit(-1);
-	}
+
 
 	//TODO: while get new line until EOF
 	int line_num = 0;
